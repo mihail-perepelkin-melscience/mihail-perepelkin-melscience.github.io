9139d8b83eaf0ee4e561f36de42752efdb2d9adf
mihail.perepelkin <mihail.perepelkin@gmail.com>
2021-09-13 00:59:55 +0300

    Updated build

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,22 +1,32 @@
 
-function setLightAmount() {
-    var lightSlider = document.getElementById("lightSlider");
-    var lightAmount = lightSlider.value / 100.0;
+var globalSimulationParameters = {
+    light : 100,
+    water : 100,
+    ground : 0,
+    fertilizer : false,
 
-    setLightFunction(lightAmount);
+    maxPlantSize : 100,
+    lifeDuration : 30,
+    leavesColor : 0xff00ff00,
+    deathType : 0
+};
+
+function updateSimulationParameters() {
+    // TODO: check for undefined document.getElementById
+    globalSimulationParameters.light = parseInt(document.getElementById("lightSlider").value);
+    //alert(document.getElementById("lightSlider").value);
+    //globalSimulationParameters.light = (int)document.getElementById("lightSlider").value;
+    //alert(typeof(document.getElementById("lightSlider").value));
+
+    updateParametersFunction();
 }
 
-function setWaterAmount() {
-    var waterSlider = document.getElementById("waterSlider");
-    var waterAmount = waterSlider.value / 100.0;
-
-    setWaterFunction(waterAmount);
+function playSimulation(timeScale) {
+    // TODO: check for undefined
+    playFunction(timeScale);
 }
 
-function setGroundType(groundType) {
-    setGroundFunction(groundType);
-}
-
-function restartSimulation() {
-    restartFunction();
+function restartSimulation(timeScale) {
+    // TODO: check for undefined
+    restartFunction(timeScale);
 }
